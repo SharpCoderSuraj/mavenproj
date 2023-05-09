0c9dcb6c1089f4fa89b230ef86d47bfa251a8cc1
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2023-05-09 09:41:18 -0300

    Estudo de Gradle concluido

---
+++ GradleProject/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld{
+
+    public static void main(String[] args){
+        System.out.println("Ola mundo");
+        
+        System.out.println("O tempo local e: " + new LocalTime());
+        
+    }
+}
