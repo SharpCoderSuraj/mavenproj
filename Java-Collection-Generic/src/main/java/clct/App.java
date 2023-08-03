ffdce9a1fd2455cf213921593ccb20ffe363a76e
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2023-08-03 13:56:08 -0300

    feat: estudo de collections

---
+++ Java-Collection-Generic/src/main/java/clct/App.java
@@ -0,0 +1,8 @@
+package clct;
+
+public class App {
+     
+    public static void main(String args[]){
+            
+        }
+}
---
+++ Java-Collection-Generic/src/main/java/clct/generic/Generico.java
@@ -0,0 +1,11 @@
+package clct.generic;
+
+public class Generico<T> {
+    private T obj;
+
+    public <T> void mandarOi(T t){
+        System.out.println(t);
+    }
+
+
+}
