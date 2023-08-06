177c1c88d1901d97494966d2747889a9fc12d1f4
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2023-08-06 12:45:27 -0300

    feat: streams

---
+++ Streams/src/main/java/streams/SupplierExemple.java
@@ -0,0 +1,10 @@
+package streams;
+
+import java.util.function.Supplier;
+
+public class SupplierExemple {
+    public void run(){
+        Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)";
+
+    }
+}
